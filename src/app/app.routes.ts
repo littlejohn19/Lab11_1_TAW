a48887559736da37e505481c69897688deb50aa4
Darek <darekpiwko@outlook.com>
2024-05-28 22:15:12 +0200

    add project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from "./components/blog/blog.component";
+import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
+import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: BlogHomeComponent,
+  },
+  {
+    path: 'blog/detail/:id',
+    component: BlogItemDetailsComponent
+  }
+];
+
---
+++ src/app/components/blog/blog.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {DataService} from "../../services/data.service";
+import {BlogItemComponent} from "../blog-item/blog-item.component";
+import {CommonModule} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
+import {SearchBarComponent} from "../search-bar/search-bar.component";
+import {FilterTextPipe} from "../../pipes/filter-text.pipe";
+
+@Component({
+  selector: 'app-blog',
+  standalone: true,
+  imports: [BlogItemComponent, CommonModule, SearchBarComponent, FilterTextPipe],
+  providers: [DataService],
+  templateUrl: './blog.component.html',
+  styleUrl: './blog.component.css'
+})
+export class BlogComponent implements OnInit {
+
+  @Input() filterText: string = '';
+
+  public items$: any;
+
+  constructor(private service: DataService) {
+  }
+  ngOnInit() {
+    this.getAll();
+  }
+
+  getAll(){
+    this.service.getAll().subscribe(response => {
+      this.items$ = response;
+    });
+  }
+}
+
